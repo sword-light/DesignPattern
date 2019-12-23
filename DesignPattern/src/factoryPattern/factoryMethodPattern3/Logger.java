ffffbace732d52b6b1a07e522f1083bb2393196d
wuwenqi <wuwenqi@inspur.com>
2019-12-23 15:54:17 +0800

    Factory Method Pattern
    工厂方法模式设计日志记录器：（3）通过XML文件配置日志记录器记录日志的方式。

---
+++ DesignPattern/src/factoryPattern/factoryMethodPattern3/Logger.java
@@ -0,0 +1,11 @@
+package factoryPattern.factoryMethodPattern3;
+
+/**
+ * 日志记录器接口：抽象产品
+ * @author wuwenqi
+ */
+public interface Logger {
+	
+	public void writeLog();
+	
+}
---
+++ DesignPattern/src/factoryPattern/factoryMethodPattern3/LoggerFactory.java
@@ -0,0 +1,11 @@
+package factoryPattern.factoryMethodPattern3;
+
+/**
+ * 日志记录器工厂接口：抽象工厂
+ * @author wuwenqi
+ */
+public interface LoggerFactory {
+	
+	public Logger createLogger();
+	
+}

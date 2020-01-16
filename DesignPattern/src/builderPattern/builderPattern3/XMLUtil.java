1f188113bab59f269fb1fae63dc6d5159002534e
wuwenqi <wuwenqi@inspur.com>
2020-01-16 14:25:04 +0800

    Builder Pattern
    builderPattern3

--- DesignPattern/src/builderPattern/builderPattern3/XMLUtil.java
+++ DesignPattern/src/builderPattern/builderPattern3/XMLUtil.java
@@ -13,7 +13,7 @@ public class XMLUtil {
 			DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = dFactory.newDocumentBuilder();
 			Document doc;
-			doc = builder.parse(new File("src/builderPattern/builderPattern1/config.xml"));
+			doc = builder.parse(new File("src/builderPattern/builderPattern3/config.xml"));
 
 			// 获取包含类名的文本节点
 			NodeList nl = doc.getElementsByTagName("className");
